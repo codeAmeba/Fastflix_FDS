d6e43b5917d2ca38224020efbd8ba90ebb2edc4a
Sub2n <95su1208@gmail.com>
2019-07-11 17:07:33 +0900

    feat: main header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
+  styleUrls: ['./app.component.css']
+  })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    this.navigate();
+    // this.navigate();
   }
 
   navigate() {
-    const token = JSON.parse(localStorage.getItem('login'));
+    const token = JSON.parse(localStorage.getItem('login'))
 
-    // if (token) this.router.navigate(['home']);
-    // else this.router.navigate(['welcome']);
+    if (token) this.router.navigate(['home'])
+    else this.router.navigate(['welcome'])
   }
 }
