6199e1747c6233d6c3e4b2258c3745dcc52a2c1a
Sub2n <95su1208@gmail.com>
2019-07-11 18:02:23 +0900

    feat: added thanos row

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
-})
+  })
 export class HomeComponent implements OnInit {
+  user: string
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.user = '사용자'
   }
-
 }
