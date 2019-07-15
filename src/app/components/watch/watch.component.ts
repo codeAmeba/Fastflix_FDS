d1d82156b4a9159a63f03082678f9f54d36e3eea
jeongsooyoung <code.ameba@gmail.com>
2019-07-15 16:16:57 +0900

    added watch component

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
+declare let videojs: any;
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
   styleUrls: ['./watch.component.css']
 })
-export class WatchComponent implements OnInit {
+export class WatchComponent implements AfterViewInit {
+ vidObj: any;
 
-  constructor() { }
+  // tslint:disable-next-line: max-line-length
+  poster = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/poster.png';
+  video = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4';
 
-  ngOnInit() {
+  @ViewChild('myvid', null) vid: ElementRef;
+
+  ngAfterViewInit() {
+    const options = {
+      controls: true,
+      autoplay: false,
+      preload: 'auto',
+      techOrder: ['html5']
+    };
+
+    this.vidObj = new videojs(this.vid.nativeElement, options, function onPlayerReady() {
+      videojs.log('player is working!');
+    });
+
+    const myPlayer = videojs('my-video');
+    myPlayer.src({ type: 'video/mp4', 
+                    src: this.video });
+
+    myPlayer.ready(() => {
+      myPlayer.currentTime(120); // parameter에 재생된 시간 넣을 방법 강구
+      videojs.log(myPlayer.currentTime());
+      // console.log(myPlayer.duration());
+    });
+
+
+
   }
-
 }
