0344cec87294f47964b0f1d7211627feb689de89
Sub2n <95su1208@gmail.com>
2019-07-16 16:52:58 +0900

    feat: fix header directive

--- src/app/directives/fix-header.directive.ts
+++ src/app/directives/fix-header.directive.ts
@@ -5,13 +5,21 @@ import { Router } from '@angular/router';
   selector: '[fixHeader]',
 })
 export class FixHeaderDirective {
+  lastScrollTop: number = 0;
+  state: string = '';
+
   constructor(
     private el: ElementRef,
     private renderer: Renderer2,
     private router: Router
   ) {}
 
-  @HostListener('window:scroll') fix() {
+  @HostListener('window:scroll', ['$event']) fix() {
+    console.log(this.lastScrollTop, window.pageYOffset);
+
+    if (this.lastScrollTop > window.pageYOffset) this.state = 'up';
+    else this.state = 'down';
+
     if (window.pageYOffset >= 10) {
       if (this.router.url === '/home') {
         // top: 0px; position: fixed; background: rgb(20, 20, 20);
@@ -23,14 +31,25 @@ export class FixHeaderDirective {
           'rgb(20, 20, 20)'
         );
       } else {
-        // top: -68px; position: fixed; background: rgb(20, 20, 20);
-        this.renderer.setStyle(this.el.nativeElement, 'top', '-68px');
-        this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
-        this.renderer.setStyle(
-          this.el.nativeElement,
-          'background',
-          'rgb(20, 20, 20)'
-        );
+        if (this.state === 'down') {
+          // top: -68px; position: fixed; background: rgb(20, 20, 20);
+          this.renderer.setStyle(this.el.nativeElement, 'top', '-68px');
+          this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
+          this.renderer.setStyle(
+            this.el.nativeElement,
+            'background',
+            'rgb(20, 20, 20)'
+          );
+        } else {
+          // top: -68px; position: fixed; background: rgb(20, 20, 20);
+          this.renderer.setStyle(this.el.nativeElement, 'top', '0');
+          this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
+          this.renderer.setStyle(
+            this.el.nativeElement,
+            'background',
+            'rgb(20, 20, 20)'
+          );
+        }
       }
     } else {
       // top: 0px; position: relative; background: transparent;
@@ -42,5 +61,7 @@ export class FixHeaderDirective {
         'transparent'
       );
     }
+
+    this.lastScrollTop = window.pageYOffset;
   }
 }
