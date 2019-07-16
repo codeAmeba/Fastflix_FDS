6b4e0e333d47deb4288127bbc925714c0aeb5514
Sub2n <95su1208@gmail.com>
2019-07-16 16:36:45 +0900

    feat: bootstrap error fixed

--- src/app/directives/fix-header.directive.ts
+++ src/app/directives/fix-header.directive.ts
@@ -1,26 +1,46 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core'
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Directive({
-  selector: '[fixHeader]'
-  })
+  selector: '[fixHeader]',
+})
 export class FixHeaderDirective {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private router: Router
+  ) {}
 
   @HostListener('window:scroll') fix() {
     if (window.pageYOffset >= 10) {
-      // top: 0px; position: fixed; background: rgb(20, 20, 20);
-      this.renderer.setStyle(this.el.nativeElement, 'top', '0')
-      this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed')
+      if (this.router.url === '/home') {
+        // top: 0px; position: fixed; background: rgb(20, 20, 20);
+        this.renderer.setStyle(this.el.nativeElement, 'top', '0');
+        this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
+        this.renderer.setStyle(
+          this.el.nativeElement,
+          'background',
+          'rgb(20, 20, 20)'
+        );
+      } else {
+        // top: -68px; position: fixed; background: rgb(20, 20, 20);
+        this.renderer.setStyle(this.el.nativeElement, 'top', '-68px');
+        this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
+        this.renderer.setStyle(
+          this.el.nativeElement,
+          'background',
+          'rgb(20, 20, 20)'
+        );
+      }
+    } else {
+      // top: 0px; position: relative; background: transparent;
+      this.renderer.setStyle(this.el.nativeElement, 'top', '0');
+      this.renderer.setStyle(this.el.nativeElement, 'position', 'relative');
       this.renderer.setStyle(
         this.el.nativeElement,
         'background',
-        'rgb(20, 20, 20)'
-      )
-    } else {
-      // top: 0px; position: relative; background: transparent;
-      this.renderer.setStyle(this.el.nativeElement, 'top', '0')
-      this.renderer.setStyle(this.el.nativeElement, 'position', 'relative')
-      this.renderer.setStyle(this.el.nativeElement, 'background', 'transparent')
+        'transparent'
+      );
     }
   }
 }
