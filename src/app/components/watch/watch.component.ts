ace2d195fe4fac3579ab1ec2448e7d62efa62297
jeongsooyoung <code.ameba@gmail.com>
2019-07-16 20:16:39 +0900

    localstorage에 마지막 시간 저장 후 재접속 시 마지막 시간부터 시작

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -29,10 +29,24 @@ export class WatchComponent implements AfterViewInit {
     myPlayer.src({ type: 'video/mp4', 
                     src: this.video });
 
+    // *테스트* 새로고침 후 재생 시 저장된 시간부터 시작       
     myPlayer.ready(() => {
-      myPlayer.currentTime(); // parameter에 재생된 시간 넣을 방법 강구
-      videojs.log(myPlayer.currentTime());
-      // console.log(myPlayer.duration());
+      myPlayer.currentTime(localStorage.getItem('lastTime'));
+      videojs.log(`마지막으로 저장된 시간 : ${myPlayer.currentTime()} 초`);
     });
+    
+    // *테스트* beforunload 이벤트 발생 시 localstorage에 현재 시간(초) 저장
+    // 최종적으로 localstorage 대신 DB 적용해야 됨
+    window.addEventListener("beforeunload", function () {
+      const myPlayer = videojs('my-video');
+      localStorage.setItem('lastTime', myPlayer.currentTime());
+    });
+
+    // history.back 이벤트 발생 현재 시간 저장
+    // window.addEventListener("history.back", function () {
+    //   const myPlayer = videojs('my-video');
+    //   localStorage.setItem('lastTime', myPlayer.currentTime());
+    // });
   }
+
 }
