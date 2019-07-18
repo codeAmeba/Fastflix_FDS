a156fffafa4d1418bf12211923380c0d1a323990
Sub2n <95su1208@gmail.com>
2019-07-18 20:47:52 +0900

    feat: profile 등록중

--- src/app/components/signup-step3/signup-step3.component.ts
+++ src/app/components/signup-step3/signup-step3.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-step3',
   templateUrl: './signup-step3.component.html',
-  styleUrls: ['./signup-step3.component.css']
+  styleUrls: ['./signup-step3.component.css'],
 })
 export class SignupStep3Component implements OnInit {
+  profileForm: FormGroup;
+  userName: string;
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
+    this.userName = this.userService.userName;
+    console.log(this.userName);
+
+    this.profileForm = new FormGroup({
+      ownerName: new FormControl('', [Validators.required]),
+      profile1Name: new FormControl(''),
+      profile2Name: new FormControl(''),
+      profile3Name: new FormControl(''),
+      profile4Name: new FormControl(''),
+    });
   }
 
+  onSubmit() {
+    const user = {
+      name: this.profileForm.get('ownerName').value,
+      kid: false,
+    };
+    this.userService.setProfile(user).subscribe(
+      data => {
+        console.log(data);
+        this.router.navigate(['/signup/step4']);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  get ownerName() {
+    return this.profileForm.get('ownerName').value;
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,18 +3,29 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { UserProfile } from '../models/user-profile';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService implements OnInit {
   apiUrl = environment.apiUrl;
+  userName: string;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userName = '';
+  }
 
-  signup(user: User): Observable<any> {
+  signup(user: User): Observable<User> {
     return this.http.post<any>(`${this.apiUrl}/accounts/create_user/`, user);
+  }
+
+  setProfile(user: UserProfile): Observable<UserProfile> {
+    return this.http.post<UserProfile>(
+      `${this.apiUrl}/accounts/create_sub_user/`,
+      user
+    );
   }
 }
