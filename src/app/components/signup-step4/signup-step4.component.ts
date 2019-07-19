e1e29ef6ab1e7f21957bc8b5b899182695ad4b6d
Sub2n <95su1208@gmail.com>
2019-07-19 18:15:57 +0900

    feat: my list image

--- src/app/components/signup-step4/signup-step4.component.ts
+++ src/app/components/signup-step4/signup-step4.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-step4',
   templateUrl: './signup-step4.component.html',
-  styleUrls: ['./signup-step4.component.css']
+  styleUrls: ['./signup-step4.component.css'],
 })
 export class SignupStep4Component implements OnInit {
+  myListForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.myListForm = new FormGroup({});
   }
-
 }
