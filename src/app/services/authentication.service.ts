99b70a9aa6e0b99c4eefa4344e4bee3f8edb420d
Sub2n <95su1208@gmail.com>
2019-07-22 21:47:19 +0900

    feat: 영화 카테고리별로 가져와야해요

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthenticationService
+  ) {}
+
+  getMainMovie(): Observable<any> {
+    const token = this.authService.getToken();
+
+    const headers = new HttpHeaders().set('Authorization', `Token ${token}`);
+
+    return this.http.get<any>(`${this.apiUrl}/movies/genre_select_before/`, {
+      headers,
+    });
+  }
 }
