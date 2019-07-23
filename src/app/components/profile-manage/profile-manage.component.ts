1292f345c2536d4b04b0778724e355cdce1f0cb3
SeungHyunKim85 <kshrtg85@gmail.com>
2019-07-23 21:47:52 +0900

    feat: profile-manage more edit

--- src/app/components/profile-manage/profile-manage.component.ts
+++ src/app/components/profile-manage/profile-manage.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileManageComponent implements OnInit {
 
-  isManage: boolean;
+  isManage: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.isManage = '';
   }
 
 }
