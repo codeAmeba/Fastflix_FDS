dc35fe6c2cf93de6e51675c76f8b1d18fc7c0cac
Sub2n <95su1208@gmail.com>
2019-07-25 20:33:20 +0900

    feat: transition works

--- src/app/directives/moveupdown.directive.ts
+++ src/app/directives/moveupdown.directive.ts
@@ -1,55 +1,93 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  Input,
+} from '@angular/core';
+import { MovieCategories } from 'src/app/models/movieCategories';
 
 @Directive({
   selector: '[Moveupdown]',
 })
 export class MoveupdownDirective {
+  @Input() category: string;
+  @Input() openedCategory: string;
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  isDown() {
+    if (!this.openedCategory) return false;
+
+    const categories = MovieCategories.map(({ category }) => category);
+
+    return (
+      categories.indexOf(this.category) >
+      categories.indexOf(this.openedCategory)
+    );
+  }
+
+  setTransition = (offset: number) => {};
+
   @HostListener('click') move() {
-    // console.log(this.el.nativeElement.closest('.slider'));
     const slider = this.el.nativeElement.closest('.slider');
     const sliderTop = slider.getBoundingClientRect().top;
     const mainView = this.el.nativeElement.closest('.mainView');
+    const thanos = document.querySelector('.thanos');
     const header = document.querySelector('.header-container');
 
-    console.log('position', sliderTop);
-    console.log('header', header);
-    console.log('thanos');
+    const detailHeight = document
+      .querySelector('.jawBoneContainer')
+      .getBoundingClientRect().height;
 
-    // this.renderer.setStyle(mainView);
-    // transform: translate3d(0px, {}px, 0px); transition-duration: 540ms; transition-delay: 50ms;
-    console.log('before', window.pageYOffset);
-    this.renderer.setStyle(
-      mainView,
-      'transform',
-      `translate3d(0px, -${sliderTop - 70}px, 0px)`
-    );
-    console.log('transition', window.pageYOffset);
-    this.renderer.setStyle(mainView, 'transition-duration', '540ms');
-    this.renderer.setStyle(mainView, 'transition-delay', '50ms');
+    if (thanos.classList.contains('has-open-jaw') && this.isDown()) {
+      // 열려있고 아래에 있으면
+      console.log('Has open jaw');
 
-    // top: 1160.8px; position: absolute; background: rgb(20, 20, 20);
+      this.renderer.setStyle(
+        mainView,
+        'transform',
+        `translate3d(0px, -${sliderTop - 70 - detailHeight}px, 0px)`
+      );
+      this.renderer.setStyle(mainView, 'transition-duration', '540ms');
+      this.renderer.setStyle(mainView, 'transition-delay', '50ms');
 
-    setTimeout(() => {
-      this.renderer.removeStyle(mainView, 'transform');
-      this.renderer.removeStyle(mainView, 'transition-duration');
-      this.renderer.removeStyle(mainView, 'transition-delay');
+      setTimeout(() => {
+        this.renderer.removeStyle(mainView, 'transform');
+        this.renderer.removeStyle(mainView, 'transition-duration');
+        this.renderer.removeStyle(mainView, 'transition-delay');
 
-      console.log('after', window.pageYOffset);
+        window.scroll({
+          top: window.pageYOffset + sliderTop - 70 - detailHeight,
+          left: 0,
+        });
 
-      window.scroll({
-        top: window.pageYOffset + sliderTop - 70,
-        left: 0,
-      });
+        this.renderer.setStyle(header, 'top', '0');
+        this.renderer.setStyle(header, 'position', 'fixed');
+        this.renderer.setStyle(header, 'background', 'rgb(20, 20, 20)');
+      }, 1000);
+    } else {
+      // 열려있지 않거나 위에 있으면
+      console.log('No open jaw');
 
-      // this.renderer.setStyle(
-      //   header,
-      //   'top',
-      //   `${window.pageYOffset + sliderTop + 70}px`
-      // );
-      // this.renderer.setStyle(header, 'position', 'absolute');
-      // this.renderer.setStyle(header, 'background', 'rgb(20, 20, 20)');
-    }, 1000);
+      this.renderer.setStyle(
+        mainView,
+        'transform',
+        `translate3d(0px, -${sliderTop - 70}px, 0px)`
+      );
+      this.renderer.setStyle(mainView, 'transition-duration', '540ms');
+      this.renderer.setStyle(mainView, 'transition-delay', '50ms');
+
+      setTimeout(() => {
+        this.renderer.removeStyle(mainView, 'transform');
+        this.renderer.removeStyle(mainView, 'transition-duration');
+        this.renderer.removeStyle(mainView, 'transition-delay');
+
+        window.scroll({
+          top: window.pageYOffset + sliderTop - 70,
+          left: 0,
+        });
+      }, 1000);
+    }
   }
 }
