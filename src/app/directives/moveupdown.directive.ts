95ff7635cecd19d64d034af318213be7b04e178d
Sub2n <95su1208@gmail.com>
2019-07-25 19:24:02 +0900

    feat: transition시 mainView 잘림 문제 해결

--- src/app/directives/moveupdown.directive.ts
+++ src/app/directives/moveupdown.directive.ts
@@ -11,7 +11,6 @@ export class MoveupdownDirective {
     const slider = this.el.nativeElement.closest('.slider');
     const sliderTop = slider.getBoundingClientRect().top;
     const mainView = this.el.nativeElement.closest('.mainView');
-    const thanos = this.el.nativeElement.closest('.thanos');
     const header = document.querySelector('.header-container');
 
     console.log('position', sliderTop);
@@ -20,26 +19,24 @@ export class MoveupdownDirective {
 
     // this.renderer.setStyle(mainView);
     // transform: translate3d(0px, {}px, 0px); transition-duration: 540ms; transition-delay: 50ms;
+    console.log('before', window.pageYOffset);
     this.renderer.setStyle(
       mainView,
       'transform',
       `translate3d(0px, -${sliderTop - 70}px, 0px)`
     );
+    console.log('transition', window.pageYOffset);
     this.renderer.setStyle(mainView, 'transition-duration', '540ms');
     this.renderer.setStyle(mainView, 'transition-delay', '50ms');
 
     // top: 1160.8px; position: absolute; background: rgb(20, 20, 20);
 
-    if (thanos.classList.contains('has-open-jaw')) {
-      thanos.classList.remove('has-open-jaw');
-    } else {
-      thanos.classList.add('has-open-jaw');
-    }
-
     setTimeout(() => {
       this.renderer.removeStyle(mainView, 'transform');
       this.renderer.removeStyle(mainView, 'transition-duration');
       this.renderer.removeStyle(mainView, 'transition-delay');
+
+      console.log('after', window.pageYOffset);
 
       window.scroll({
         top: window.pageYOffset + sliderTop - 70,
