790de70b0a9227e0c68319a9a21139db9fcee9bd
jeongsooyoung <code.ameba@gmail.com>
2019-07-30 13:25:06 +0900

    added missing items

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -10,16 +10,23 @@ declare let videojs: any;
 export class WatchComponent implements AfterViewInit, OnDestroy {
   vidObj: any;
   poster: string = 'https://i.ytimg.com/vi/YE7VzlLtp-4/maxresdefault.jpg';
+  
+  // 샘플 영상 링크
   video: string = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
   isInactive: boolean;
   pauseMovie: boolean;
 
-  // 데이터 받았다고 가정함
+  // 영화정보 데이터 받았다고 가정함
   movieTitle: string = 'Big Buck Bunny';
-  year: number = 2019;
-  old: string = "All";
-  runningTime: string = '09:56';
+  madeYear: number = 2019;
+  ageLimit: string = '12';
+  runningTime: string = '9분 56초';
   movieIntro: string = 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna.';
+
+  // 시간 저장 변수
+  hourOfMovie: number;
+  minOfMovie: number;
+  secOfMovie: number; 
 
   @ViewChild('myvid', null) vid: ElementRef;
   
@@ -73,12 +80,19 @@ export class WatchComponent implements AfterViewInit, OnDestroy {
       myControlBar.appendChild(newDiv);
       newDiv.classList.add('back-forward-contain');
       newDiv.innerHTML = newButton;
+      
       document.querySelector('#moveback').addEventListener('click', this.moveBack);
       document.querySelector('#moveforward').addEventListener('click', this.moveForward);
 
+
+      //test
       // 플레이어 구동 시 lastTime부터 플레이 시작
       myPlayer.currentTime(localStorage.getItem('lastTime'));
-      videojs.log(`마지막으로 저장된 시간 : ${myPlayer.currentTime()} 초`);
+      this.hourOfMovie = this.minOfMovie > 60 ? this.minOfMovie / 60 : 0;
+      this.minOfMovie = Math.round(myPlayer.currentTime() / 60);
+      this.secOfMovie = Math.round(myPlayer.currentTime() % 60);
+      videojs.log(`마지막으로 저장된 시간 : ${ this.hourOfMovie < 10 ? '0'+this.hourOfMovie : this.hourOfMovie }:${ this.minOfMovie < 10 ? '0'+this.minOfMovie : this.minOfMovie }:${ this.secOfMovie < 10 ? '0'+this.secOfMovie : this.secOfMovie }`);
+      // videojs.log(`마지막으로 저장된 시간 : ${Math.round(myPlayer.currentTime())} 초`);
     });         
 
     // *테스트* beforunload 이벤트(새로고침, url 변경) 발생 시 localstorage에 현재 시간(초) 저장
