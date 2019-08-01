503e149e9c786ec0ed8b3e523ca4ea2dc7244f38
Sub2n <95su1208@gmail.com>
2019-08-01 12:55:49 +0900

    feat: get My List

--- src/app/components/my-list/my-list.component.ts
+++ src/app/components/my-list/my-list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
+import { MoviePreview } from 'src/app/models/movie-preview';
 
 @Component({
   selector: 'app-my-list',
   templateUrl: './my-list.component.html',
-  styleUrls: ['./my-list.component.css']
+  styleUrls: ['./my-list.component.css'],
 })
 export class MyListComponent implements OnInit {
+  myLists: MoviePreview[];
+  sliderLines: number;
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit() {
+    this.getMyListMovies();
   }
 
+  getMyListMovies() {
+    this.movieService.getMyListMovies().subscribe(movies => {
+      this.myLists = movies.map(movie => {
+        return {
+          id: movie.id,
+          title: movie.name,
+          url: movie['horizontal_image_path'],
+          preview: movie['sample_video_file'],
+        };
+      });
+      console.log('내가 찜한 목록', this.myLists);
+      this.parseMyList();
+    });
+  }
+
+  parseMyList() {
+    this.sliderLines = Math.ceil(this.myLists.length / 6);
+  }
 }
