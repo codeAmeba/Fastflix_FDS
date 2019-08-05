f782abf3f7f4487215e91208763142aa387a6e78
Sub2n <95su1208@gmail.com>
2019-08-05 14:34:26 +0900

    feat: detail hover watch, mylsit

--- src/app/components/detail-slider/detail-slider.component.ts
+++ src/app/components/detail-slider/detail-slider.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { similarMovies } from "src/app/models/movies-detail";
+import { Component, OnInit, Input } from '@angular/core';
+import { similarMovies, MovieDetail } from 'src/app/models/movies-detail';
+import { MovieService } from 'src/app/services/movie.service';
 @Component({
-  selector: "app-detail-slider",
-  templateUrl: "./detail-slider.component.html",
-  styleUrls: ["./detail-slider.component.css"]
+  selector: 'app-detail-slider',
+  templateUrl: './detail-slider.component.html',
+  styleUrls: ['./detail-slider.component.css'],
 })
 export class DetailSliderComponent implements OnInit {
   @Input() similarContent: similarMovies[];
-  constructor() {}
+
+  constructor(private movieService: MovieService) {}
 
   ngOnInit() {}
+
+  myList(movie: similarMovies) {
+    this.movieService.myList(movie.id).subscribe(({ marked }) => {
+      console.log('myList', movie.id, marked);
+      movie.marked = marked;
+    });
+  }
 }
