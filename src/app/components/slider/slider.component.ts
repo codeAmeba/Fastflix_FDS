7e35fcf95da829b29059be0bb72676e94fb1ef99
Sub2n <95su1208@gmail.com>
2019-08-08 19:02:17 +0900

    feat: 시청중 슬라이더 오류

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -103,7 +103,8 @@ export class SliderComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    console.log(this.isContinue, this.moviesList);
+    if (this.isContinue)
+      console.log('시청중', this.isContinue, this.moviesList);
 
     this.movies = this.moviesList.map((movie, index) => ({
       ...movie,
